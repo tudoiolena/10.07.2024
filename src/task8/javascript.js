2d8f384b0d0d43ccd00af2f11a527d8f55954bac
Olena Tudoi <olena.tudoi@gmail.com>
2024-07-11 21:02:19 +0300

    NEW: Add task8

---
+++ src/task8/javascript.js
@@ -0,0 +1,28 @@
+// Записати вміст змінної sysInfo за допомогою стріма в файл
+// const proess = require("node:process");
+// const sysInfo = JSON.stringify(proess);
+
+const proess = require("node:process");
+
+const fs = require("node:fs");
+const path = require("node:path");
+const stream = require("node:stream");
+
+const CURR_DIR = __dirname;
+const DATA_DIR = path.resolve(CURR_DIR, "data");
+
+const sysInfo = JSON.stringify(proess);
+
+const sysInfoCopyFileName = "copied-sysinfo.json";
+const sysInfoCopyFile = path.resolve(DATA_DIR, sysInfoCopyFileName);
+
+const readableStream = new stream.Readable({ read(size) {} });
+readableStream.push(sysInfo);
+
+const writableStream = fs.createWriteStream(sysInfoCopyFile);
+
+writableStream.on("error", (err) => {
+  console.error("Error writing to file:", err);
+});
+
+readableStream.pipe(writableStream);
